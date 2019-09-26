ec7f0280aa1aadb40701c5771881c95f7ff30d52
Giovanny Chaves <giovanny.chaves@enext.com.br>
2019-09-26 07:07:10 -0700

    Estilização do light box concluida

--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -3,7 +3,7 @@ function loadText(){
     xhr.onreadystatechange = function (){
         if(this.readyState = 4 && this.status == 200){
             var data = JSON.parse(this.responseText);
-            console.log(data);
+            // console.log(data);
             showTxt(data);
         }
     }
@@ -13,7 +13,42 @@ function loadText(){
 
 loadText();
 
+const portionImage = document.querySelector('.potion-image');
+const potionName = document.querySelector(".potion-name");
+const potionEffect = document.querySelector(".potion-effect");
+const potionIngre = document.querySelector(".potion-ingredients");
+const potionPrice = document.querySelector(".potion-price");
+const modal = document.querySelector("#myModal");
+const btnShow = document.querySelector(".product");
+const btnClose = document.getElementsByClassName("close")[0];
+
 function showTxt(data){
-    const useDiv = document.querySelector(".potion-name");
-    useDiv.innerHTML = data.potions[1].name;
+
+    for (let i = 0; i < data.length; i++) {
+        console.log(data[i])
+    }
+    
+    portionImage.src=`src/img/products/${data.potions[1].image}`;
+    potionName.innerHTML = `${data.potions[1].name} <br><br>`;
+    potionEffect.innerHTML = `${data.potions[1].effect} <br><br>`;
+    potionIngre.innerHTML = `${data.potions[1].ingredients.join(', <br>')} <br><br> `;
+    potionPrice.innerHTML = `$${data.potions[1].price} <br><br>`;
+}
+
+
+// When the user clicks the button, open the modal 
+btnShow.onclick = function() {
+    modal.style.display='block';
+}
+
+// When the user clicks on <span> (x), close the modal
+btnClose.onclick = function() {
+    modal.style.display='none';
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display='none';
+  }
 }
