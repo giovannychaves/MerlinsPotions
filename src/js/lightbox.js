5b9498a789b6d3772dd78dc30a45481cec7d29e4
Giovanny Chaves <giovanny.chaves@enext.com.br>
2019-09-26 11:56:23 -0700

    Ajuste no js de lightbox removendo o erro com o onLoad

--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -9,11 +9,12 @@ const btnShow = document.querySelector(".products");
 const btnClose = document.getElementsByClassName("close")[0];
 
 function loadText(){
+    
     var xhr = new XMLHttpRequest;
-    xhr.onreadystatechange = function (){
+    xhr.onload = function (){
         if(this.readyState = 4 && this.status == 200){
             let data = JSON.parse(this.responseText);
-            // console.log(data);
+            console.log(data);
             showTxt(data);
         }
     }
