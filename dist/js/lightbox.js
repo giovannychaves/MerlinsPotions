d14317794d910821919fff2d16b31b8c45ad3367
Giovanny Chaves <giovanny.chaves@enext.com.br>
2019-10-10 19:56:11 -0300

    gulp

--- dist/js/lightbox.js
+++ dist/js/lightbox.js
@@ -11,26 +11,31 @@ var btnShow = document.querySelector(".products");
 var btnClose = document.getElementsByClassName("close")[0];
 
 function loadText() {
-  var xhr = new XMLHttpRequest();
-
-  xhr.onload = function () {
-    if (this.readyState = 4 && this.status == 200) {
-      var data = JSON.parse(this.responseText);
-      console.log(data);
-      showTxt(data);
-    }
-  };
-
-  xhr.open('GET', 'https://cdn.rawgit.com/LucasRuy/1d4a5d45e2ea204d712d0b324af28bab/raw/342e0e9277be486102543c7f50ef5fcf193234b6/potions.json', true);
-  xhr.send();
+  fetch('https://cdn.rawgit.com/LucasRuy/1d4a5d45e2ea204d712d0b324af28bab/raw/342e0e9277be486102543c7f50ef5fcf193234b6/potions.json').then(function (response) {
+    return response.json();
+  }).then(function (response) {
+    return showTxt(response);
+  }); // let xhr = new XMLHttpRequest();
+  // xhr.onload = function (){
+  //     if(this.readyState === 4 && this.status === 200){
+  //         let data = JSON.parse(this.responseText);
+  //         console.log(data);
+  //         showTxt(data);
+  //     }
+  // }
+  // xhr.open('GET', 'https://cdn.rawgit.com/LucasRuy/1d4a5d45e2ea204d712d0b324af28bab/raw/342e0e9277be486102543c7f50ef5fcf193234b6/potions.json', true);
+  // xhr.send();  
 }
 
+;
 loadText();
 
-function showTxt(data) {
+function showTxt(response) {
+  console.log(response);
+
   var _loop = function _loop(i) {
     products[i].onclick = function () {
-      var item = data.potions[i + 1];
+      var item = response.potions[i + 1];
       portionImage.src = "src/img/products/".concat(item.image);
       potionName.innerHTML = "".concat(item.name, " <br><br>");
       potionEffect.innerHTML = "".concat(item.effect, " <br><br>");
--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,41 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
-var babel = require("gulp-babel");
+const browserSync = require("browser-sync").create();
 
-gulp.task('sass', function(){
+
+
+// SASS
+function compilaSass(){
     return gulp.src('./src/scss/style.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist/css/'));
-});
+    .pipe(sass({
+          outputStyle: 'compressed'
+      }))
+    .pipe(gulp.dest('./dist/css/'))
+    .pipe(browserSync.stream());
+};
+gulp.task('sass', compilaSass);
 
-gulp.task('babel', function(){
-    return gulp.src('./src/js/*.js')
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(gulp.dest('./dist/js/'))
-});
+
+
+// Watch
+function wacthSass(){
+    gulp.watch('./src/scss/*.scss', compilaSass);
+    gulp.watch('*.html').on('change', browserSync.reload);
+};
+gulp.task('watch', wacthSass);
+
+
+
+//Browser-sync
+function browser(){
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+};
+gulp.task('browser-sync', browser);
+
+
+
+gulp.task('default', gulp.parallel('watch', 'browser-sync'));
